0724be33dc99c31567c915a32ace1bee17497e9e
Vlad <ysc.vlad@gmail.com>
2018-05-28 16:13:08 +0300

    change code style in CheckOrder class

--- task2/CheckOrder.java
+++ task2/CheckOrder.java
@@ -1,14 +1,22 @@
 package mainPackage;
 
 public class CheckOrder {
+    private static int firstNumeral,
+                        secondNumeral,
+                        thirdNumeral,
+                        forthNumral;
+
+    private static void findNumrals(int value){
+        firstNumeral = value % 10;
+        value /= 10;
+        secondNumeral = value % 10;
+        value /= 10;
+        thirdNumeral = value % 10;
+        value /= 10;
+        forthNumral = value;
+    }
     public static boolean checkOrderInNumber(int value){
-        int firstNumeral = value % 10;
-        value /= 10;
-        int secondNumeral = value % 10;
-        value /= 10;
-        int thirdNumeral = value % 10;
-        value /= 10;
-        int forthNumral = value;
+        findNumrals(value);
         boolean orderOrNot = ((firstNumeral > secondNumeral) &&
                                 (secondNumeral > thirdNumeral) &&
                                 (thirdNumeral > forthNumral)) ||
