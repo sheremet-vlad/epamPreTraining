3a42288ddc9b1decd7b2f59b0845222998462c6f
Vlad <ysc.vlad@gmail.com>
2018-06-20 03:49:06 +0300

    add class Main

---
+++ task7/Main.java
@@ -0,0 +1,9 @@
+package by.epam.preTraining.vladSheremet.tasks.task7;
+
+import by.epam.preTraining.vladSheremet.tasks.task7.controller.Test;
+
+public class Main {
+    public static void main(String[] args) {
+        Test.test();
+    }
+}
